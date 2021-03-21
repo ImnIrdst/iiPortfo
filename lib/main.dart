96e2a4472e797e1e8442125b02701c073189d3b9
Iman Irandoost <imn.irdst@gmail.com>
2021-03-21 07:28:24 +0330

    Use amber theme

--- lib/main.dart
+++ lib/main.dart
@@ -4,16 +4,32 @@ void main() {
   runApp(MyApp());
 }
 
+final primaryColor = Colors.amber;
+
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'iiPortfo',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
+          primarySwatch: primaryColor,
+          accentColor: primaryColor,
+          splashColor: Colors.deepOrange,
+          canvasColor: Colors.black,
+          buttonTheme: _getButtonTheme(context),
+          appBarTheme: _getAppBarTheme(context),
+          visualDensity: VisualDensity.adaptivePlatformDensity,
+          brightness: Brightness.dark),
       home: MyHomePage(title: 'iiPortfo'),
     );
+  }
+
+  ButtonThemeData _getButtonTheme(context) {
+    return Theme.of(context).buttonTheme.copyWith(buttonColor: primaryColor);
+  }
+
+  AppBarTheme _getAppBarTheme(context) {
+    return Theme.of(context).appBarTheme.copyWith(color: Colors.black);
   }
 }
 
