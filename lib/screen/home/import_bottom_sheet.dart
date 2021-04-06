fdebc60ab830ea5c2e6ae544a7ecea4e82d90cf5
Iman Irandoost <imn.irdst@gmail.com>
2021-04-06 19:21:55 +0430

    Design Import bottomsheet

--- lib/screen/home/import_bottom_sheet.dart
+++ lib/screen/home/import_bottom_sheet.dart
@@ -4,20 +4,56 @@ class ImportBottomSheet extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: 200,
-      color: Colors.grey[900],
-      child: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          mainAxisSize: MainAxisSize.min,
-          children: <Widget>[
-            const Text('Modal BottomSheet'),
-            ElevatedButton(
-              child: const Text('Close BottomSheet'),
-              onPressed: () => Navigator.pop(context),
-            )
-          ],
-        ),
+      height: 400,
+      padding: EdgeInsets.symmetric(vertical: 16),
+      // color: Colors.grey[900],
+      decoration: BoxDecoration(
+        color: Colors.grey[900],
+        border: Border.all(),
+        borderRadius: BorderRadius.all(Radius.circular(20)),
+      ),
+      child: ListView(
+        children: [
+          Container(
+            margin: EdgeInsets.fromLTRB(184, 0, 184, 32),
+            height: 4,
+            color: Colors.grey[700],
+          ),
+          ImportItem(title: "Custom CSV"),
+          ImportItem(title: "Nobitex"),
+        ],
+      ),
+    );
+  }
+}
+
+class ImportItem extends StatelessWidget {
+  final String title;
+
+  const ImportItem({Key key, this.title}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(8),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Icon(Icons.table_chart_outlined),
+              ),
+              Text(title),
+            ],
+          ),
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Icon(Icons.info_outline),
+          ),
+        ],
       ),
     );
   }
