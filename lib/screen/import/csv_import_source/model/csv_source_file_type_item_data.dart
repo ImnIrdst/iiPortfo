661c62c8402facbd15e55b47359ad2d0bc74c9fe
Iman Irandoost <imn.irdst@gmail.com>
2021-05-07 10:05:10 +0430

    Make CSV source item data readable and writable to json

--- lib/screen/import/csv_import_source/model/csv_source_file_type_item_data.dart
+++ lib/screen/import/csv_import_source/model/csv_source_file_type_item_data.dart
@@ -4,6 +4,15 @@ class CsvSourceFileTypeItemData {
 
   CsvSourceFileTypeItemData(this.id, this.name);
 
+  CsvSourceFileTypeItemData.fromJson(Map<String, dynamic> json)
+      : id = json['id'],
+        name = json['name'];
+
+  Map<String, dynamic> toJson() => {
+        'id': id,
+        'name': name,
+      };
+
   static final supportedItems = [
     CsvSourceFileTypeItemData("nobitex", "Nobitex Transactions"),
     CsvSourceFileTypeItemData("bitpay", "Bitpay Transactions"),
--- lib/screen/import/csv_import_source/model/csv_source_item_data.dart
+++ lib/screen/import/csv_import_source/model/csv_source_item_data.dart
@@ -15,8 +15,33 @@ class CsvImportSourceItemData extends ImportSourceItemData {
       filePath.isNotEmpty &&
       sourceFileType != null;
 
+  CsvImportSourceItemData.fromJson(Map<String, dynamic> json)
+      : filePath = json['file_path'],
+        sourceFileType =
+            CsvSourceFileTypeItemData.fromJson(json['source_file_type']),
+        super(json['account_name']);
+
+  Map<String, dynamic> toJson() => {
+        'account_name': accountName,
+        'file_path': filePath,
+        'source_file_type': sourceFileType,
+      };
+
   @override
   String toString() {
     return 'CsvSourceItemData{accountName: $accountName, filePath: $filePath, sourceFileType: $sourceFileType}';
+  }
+}
+
+extension CsvImportSourceItemDataListJsonConverter
+    on List<CsvImportSourceItemData> {
+  List<Map<String, dynamic>> toJson() {
+    return this.map((e) => e.toJson()).toList();
+  }
+}
+
+extension on List<Map<String, dynamic>> {
+  List<CsvImportSourceItemData> toCsvImportSourceItemDataList() {
+    return this.map((e) => CsvImportSourceItemData.fromJson(e)).toList();
   }
 }
